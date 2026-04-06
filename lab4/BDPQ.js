a91984047525f82bb366142ed952c5183b1854f1
dimpennn <dmitrijpenkivskij@gmail.com>
2026-04-06 22:57:00 +0300

    feat: implement BDPQ class with initial methods

---
+++ lab4/BDPQ.js
@@ -0,0 +1,41 @@
+class BDPQ {
+    constructor() {
+        this.s = new Set();
+    }
+
+    enqueu(item, priority) {
+        
+    }
+
+    peekHighestPriority() {
+
+    }
+
+    peekLowestPriority() {
+
+    }
+
+    peekNewest() {
+
+    }
+
+    peekOldest() {
+
+    }
+
+    dequeueHighestPriority() {
+
+    }
+
+    dequeueLowestPriority() {
+
+    }
+
+    dequeueNewest() {
+
+    }
+
+    dequeueOldest() {
+
+    }
+}
